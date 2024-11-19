002468f76eec75485a726350bfad99bf8e52ccd1
Yaroslav Markivskyi <yaroslav.markivskyi.it.2022@lpnu.ua>
2024-11-19 15:26:37 +0000

    update policies

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -167,6 +167,8 @@ data "aws_iam_policy_document" "rds" {
       "rds:DeleteDBInstance",
       "rds:ListTagsForResource",
       "rds:ModifyDBInstance",
+      "rds:AddTagsToResource",
+      "iam:CreateServiceLinkedRole"
     ]
     resources = ["*"]
   }
